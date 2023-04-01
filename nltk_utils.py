be7e07e8c2d260a8d5e9c4870d7bb6ebb16f1c22
mulugeta-89 <mulugetahail89@gmail.com>
2023-04-01 16:19:14 +0300

    chat-functionallity-added

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,20 +1,38 @@
-import nltk
-#nltk.download("punkt")
+
+
 import numpy as np
+import nltk
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
+
 def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
     return nltk.word_tokenize(sentence)
 
+
 def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
     return stemmer.stem(word.lower())
 
-def bag_of_words(tokenized_sentence, words):
-    bag = np.zeros(len(words), dtype=np.float32)
-    for idx, item in enumerate(words):
-        if item in tokenized_sentence:
-            bag[idx] = 1.0
-            
-    return bag
 
+def bag_of_words(tokenized_sentence, words):
+
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
 
--- train.py
+++ train.py
@@ -1,93 +1,127 @@
-import json
-from nltk_utils import stem, bag_of_words, tokenize
+
+
 import numpy as np
+import random
+import json
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+
+from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open("intents.json", "r") as f:
+with open('intents.json', 'r') as f:
     intents = json.load(f)
-    
+
 all_words = []
 tags = []
 xy = []
 
-for intent in intents["intents"]:
-    tag = intent["tag"]
+for intent in intents['intents']:
+    tag = intent['tag']
+    # add to tag list
     tags.append(tag)
-    for pattern in intent["patterns"]:
+    for pattern in intent['patterns']:
+        
         w = tokenize(pattern)
-        all_words += w
-        xy.append((w,tag))
+        # add to our words list
+        all_words.extend(w)
+        # add to xy pair
+        xy.append((w, tag))
 
-ignored_words = ["?", ",", ".", "!"]
-all_words = [stem(item) for item in all_words if item not in ignored_words]
+# stem and lower each word
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
 all_words = sorted(set(all_words))
-
 tags = sorted(set(tags))
 
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "unique stemmed words:", all_words)
 
+# create training data
 X_train = []
-Y_train = []
-counter = 0
+y_train = []
 for (pattern_sentence, tag) in xy:
+   
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-
     label = tags.index(tag)
-    Y_train.append(label)
+    y_train.append(label)
 
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
-X_train = (np.array(X_train))
-Y_train = (np.array(Y_train))
-
-
+# Hyper-parameters 
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+print(input_size, output_size)
 
 class ChatDataset(Dataset):
+
     def __init__(self):
         self.n_samples = len(X_train)
         self.x_data = X_train
-        self.y_data = Y_train
-    def __getitem__(self, idx):
-        return self.x_data[idx], self.y_data[idx]
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
     def __len__(self):
         return self.n_samples
-    
-#hyperparameters
-batch_size = 8
-input_size = len(X_train[0])
-hidden_size = 8
-output_size = len(Y_train)
-learning_rate = 0.001
-num_epochs = 1000
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-device = torch.device("cpu")
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-#loss and optimisers
-
+# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-
+# Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
+        
 
         outputs = model(words)
 
         loss = criterion(outputs, labels)
+        
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    if (epoch+1)%100 == 0:
-        print(f'Epoch [{(epoch+1)}/{num_epochs}],  loss: {loss.item():.4f}')
-print(f'final loss,  loss: {loss.item():.4f}')
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 
+print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
