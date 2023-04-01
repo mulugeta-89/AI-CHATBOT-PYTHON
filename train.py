0f8d70127c739107550fa26ae9db76358f501cb2
mulugeta-89 <mulugetahail89@gmail.com>
2023-04-01 11:27:00 +0300

    trained and loss calculated for every 100 epoch

--- train.py
+++ train.py
@@ -5,8 +5,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from model import NeuralNet
+
 with open("intents.json", "r") as f:
     intents = json.load(f)
+    
 all_words = []
 tags = []
 xy = []
@@ -40,7 +43,7 @@ for (pattern_sentence, tag) in xy:
 X_train = (np.array(X_train))
 Y_train = (np.array(Y_train))
 
-batch_size = 8
+
 
 class ChatDataset(Dataset):
     def __init__(self):
@@ -51,5 +54,40 @@ class ChatDataset(Dataset):
         return self.x_data[idx], self.y_data[idx]
     def __len__(self):
         return self.n_samples
+    
+#hyperparameters
+batch_size = 8
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(Y_train)
+learning_rate = 0.001
+num_epochs = 1000
+
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+device = torch.device("cpu")
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+#loss and optimisers
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+
+        outputs = model(words)
+
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    if (epoch+1)%100 == 0:
+        print(f'Epoch [{(epoch+1)}/{num_epochs}],  loss: {loss.item():.4f}')
+print(f'final loss,  loss: {loss.item():.4f}')
+
+
